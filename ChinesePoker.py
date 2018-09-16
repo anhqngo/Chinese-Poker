68a61536709c32c0182244131920268a5b9fd5c6
Jason Ngo <ngojason9@gmail.com>
2018-09-16 17:27:25 -0400

    Considering using external library deuces

--- ChinesePoker.py
+++ ChinesePoker.py
@@ -1,37 +1,37 @@
 import random
+import unittest
+import deuces
 
 numPlayers = 2
-suits_symbols = ['♠', '♦', '♥', '♣']
-suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
-ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K']
+# suits_symbols = ['♠', '♦', '♥', '♣']
+# suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+suits = ['C', 'D', 'H', 'S']
+ranks = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+
 
 class Player(object):
     def __init__(self, name, hand):
         self.name = name
         self.hand = hand
 
+
 class Card(object):
     def __init__(self, rank, suit):
         self.rank = rank
         self.suit = suit
-    
-    def __str__(self):
-        """
-        >>> card = Card('4', 'Hearts')
-        >>> print(card)
-        """
-        return self.rank + ' ' + self.suit
 
-def createDeck():
-    """
-    >>> assert(len(createDeck())==52)
-    """
+    def __str__(self):
+        return self.rank + self.suit
+
+
+def create_deck():
     Deck = []
     for s in suits:
         for r in ranks:
-            card = Card(r,s)
+            card = Card(r, s)
             Deck.append(card)
     return Deck
+
 
 def dealHand(deck):
     hand = []
@@ -41,20 +41,50 @@ def dealHand(deck):
         hand.append(_j)
     return hand
 
+
+def print_cards(deck):
+    s = ""
+    for card in deck:
+        s += " " + str(card)
+    return s
+
+
+def get_card_from_input(deck, user_input):
+    if len(user_input) > 2:
+        print("Not a valid input!")
+        print("Proper input example: 2H or KC")
+        return None
+    for item in deck:
+        if (item.rank == user_input[0]) and (item.suit == user_input[1]):
+            return item
+    return None
+
+
 class Game(object):
     def __init__(self):
         self.numPlayers = 2
         self.Players = []
-    
-        deck = createDeck()
+
+        deck = create_deck()
 
         for _i in range(numPlayers):
             name = input("Enter the name of Player " + str(_i+1) + ": ")
             hand = dealHand(deck)
-            self.Players.append(Player(name,hand))
+            self.Players.append(Player(name, hand))
 
-g=Game()
-print(len(g.Players[1].hand))
+
+class UnitTest(unittest.TestCase):
+    def test_create_deck(self):
+        self.assertEqual(len(create_deck()), 52)
+
+    def test_print_card(self):
+        card = Card('4', 'Hearts')
+        self.assertEqual(str(card), "4Hearts")
+
+
+if __name__ == '__main__':
+    unittest.main()
+
 
 # class Move(object):
 #     pass
