68466e63307edd2efdd3a8d1798dade5a7a0036c
Jason Ngo <ngojason9@gmail.com>
2018-09-29 17:22:08 -0400

    A bunch of change...

--- ChinesePoker.py
+++ ChinesePoker.py
@@ -1,6 +1,5 @@
 import random
 import unittest
-import deuces
 
 numPlayers = 2
 # suits_symbols = ['♠', '♦', '♥', '♣']
@@ -8,11 +7,16 @@ numPlayers = 2
 suits = ['C', 'D', 'H', 'S']
 ranks = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
 
+numCards = [5,5,3]
+
+poker_hand = {'Straight flush': 1, 'Four of a kind': 2}
 
 class Player(object):
     def __init__(self, name, hand):
         self.name = name
         self.hand = hand
+        self.groups = []
+
 
 
 class Card(object):
@@ -23,6 +27,8 @@ class Card(object):
     def __str__(self):
         return self.rank + self.suit
 
+    def __eq__(self, other):
+        return self.rank == other.rank and self.suit == other.suit
 
 def create_deck():
     Deck = []
@@ -42,14 +48,15 @@ def dealHand(deck):
     return hand
 
 
-def print_cards(deck):
+def print_cards(hand):
     s = ""
-    for card in deck:
+    for card in hand:
         s += " " + str(card)
     return s
 
 
-def get_card_from_input(deck, user_input):
+def get_card_from_input(user_input):
+    deck = create_deck()
     if len(user_input) > 2:
         print("Not a valid input!")
         print("Proper input example: 2H or KC")
@@ -59,7 +66,12 @@ def get_card_from_input(deck, user_input):
             return item
     return None
 
-
+def print_groups(groups):
+    newGroup = []
+    for group in groups:
+        newGroup.append(print_cards(group))
+    return newGroup
+    
 class Game(object):
     def __init__(self):
         self.numPlayers = 2
@@ -72,6 +84,29 @@ class Game(object):
             hand = dealHand(deck)
             self.Players.append(Player(name, hand))
 
+    def play(self):
+        print(self.Players[0].name + "'s cards are: ")
+        print(print_cards(self.Players[0].hand))
+        hand1 = self.Players[0].hand
+        for _i in range(2):
+            group = input(
+                "Enter {} cards that you want to be in group {}: ".format(numCards[_i], _i+1))
+            arr = group.split()
+            # print(arr)
+            arr2 = [get_card_from_input(i) for i in arr]
+            # print(arr2)
+
+            for card in arr2:
+                for other in hand1:
+                    if card==other:
+                        hand1.remove(other)
+            print("Your remaining cards are: " +
+                  print_cards(self.Players[0].hand))
+            self.Players[0].groups.append(arr2)
+        self.Players[0].groups.append(hand1)
+        print("Your three groups are: ")
+        print(print_groups(self.Players[0].groups))
+
 
 class UnitTest(unittest.TestCase):
     def test_create_deck(self):
@@ -81,9 +116,14 @@ class UnitTest(unittest.TestCase):
         card = Card('4', 'Hearts')
         self.assertEqual(str(card), "4Hearts")
 
+    def test_get_card_from_input(self):
+        self.assertEqual(get_card_from_input("2H"), Card("2", "H"))
+
 
 if __name__ == '__main__':
-    unittest.main()
+    # unittest.main()
+    g = Game()
+    g.play()
 
 
 # class Move(object):
